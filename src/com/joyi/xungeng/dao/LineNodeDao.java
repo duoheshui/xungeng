00fbb40c9fc266d8af477d810bb3d01cd33c8bf7
zhangyong <45014387@qq.com>
2014-10-15 12:05:53 +0800

    a

--- src/com/joyi/xungeng/dao/LineNodeDao.java
+++ src/com/joyi/xungeng/dao/LineNodeDao.java
@@ -2,10 +2,12 @@ package com.joyi.xungeng.dao;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import com.joyi.xungeng.db.LineNodeDBHelper;
 import com.joyi.xungeng.domain.LineNode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -33,11 +35,27 @@ public class LineNodeDao {
 		writableDatabase.insert("line_node", null, contentValues);
 	}
 
+	/**
+	 * 根据巡更路线获取该路线下的所有节点
+	 * @param lineid
+	 * @return
+	 */
 	public List<LineNode> getListByLineId(String lineid) {
-		List<LineNode> lineNodes = null;
+		List<LineNode> lineNodes = new ArrayList<LineNode>();
 
-
-
+		SQLiteDatabase readableDatabase = lineNodeDBHelper.getReadableDatabase();
+		Cursor cursor = readableDatabase.query("line_node", null, "lineid = ?", new String[]{lineid}, null, null, null);
+		if (cursor != null) {
+			LineNode lineNode = null;
+			while (cursor.moveToNext()) {
+				lineNode = new LineNode();
+				lineNode.setId(cursor.getString(cursor.getColumnIndex("id")));
+				lineNode.setLineid(cursor.getString(cursor.getColumnIndex("lineid")));
+				lineNode.setNodename(cursor.getString(cursor.getColumnIndex("nodeName")));
+				lineNode.setNfcCode(cursor.getString(cursor.getColumnIndex("nfcCode")));
+				lineNodes.add(lineNode);
+			}
+		}
 		return lineNodes;
 	}
 }
